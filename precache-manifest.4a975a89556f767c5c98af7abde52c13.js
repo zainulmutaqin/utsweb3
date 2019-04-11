2e360591e9a643d981fdd5e0543d4c790f2aa7fe
zainulmutaqin <zainulmutaqin15@gmail.com>
2019-04-11 08:24:24 +0700

    Updates

---
+++ precache-manifest.4a975a89556f767c5c98af7abde52c13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/utsweb3/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "371dd3676839bd68e7ae",
+    "url": "/utsweb3/static/js/runtime~main.0bfe0192.js"
+  },
+  {
+    "revision": "1722111aa914299916b2",
+    "url": "/utsweb3/static/js/main.d44bd323.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/utsweb3/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "1722111aa914299916b2",
+    "url": "/utsweb3/static/css/main.1f417d9f.chunk.css"
+  },
+  {
+    "revision": "77649cdb9cc81bf3f7dda94d5ec87f25",
+    "url": "/utsweb3/index.html"
+  }
+];
